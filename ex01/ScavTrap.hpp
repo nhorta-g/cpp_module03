5fbcaef6f1afeb3687be2fe2abb28573ba1f3b1d
nhorta-g <nunogra@gmail.com>
2023-07-19 10:37:35 +0100

    ex01

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -1,25 +1,8 @@
 #include "ClapTrap.hpp"
 
-class ScavTrap {
+class ScavTrap: ClapTrap {
 	private:
-		std::string	_name;
-		int			_hit_points;
-		int			_energy_points;
-		int			_attack_damage;
+
 	public:
-		ScavTrap (void);
-		ScavTrap (std::string name);
-		ScavTrap (const ScavTrap &original);
-		ScavTrap& operator = (const ScavTrap &original);
-		~ScavTrap (void);
-
-		void attack(const std::string& target);
-		void takeDamage(unsigned int amount);
-		void beRepaired(unsigned int amount);
-
-		///////GETTERS & SETTERS///////
-		std::string getName(void) const;
-		int			getHitPoints(void) const;
-		int			getEnergyPoints(void) const;
-		int			getAttackDamage(void) const;
+		void guardGate(void);
 };
